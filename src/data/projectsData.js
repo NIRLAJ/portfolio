5e9fb04e187065ac25e7fa9b7cd95ea70aaf2618
Aryan <Aryanchaudhary2629@gmail.com>
2025-05-28 22:10:23 +0530

    Updated Code

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,12 +1,3 @@
-import one from '../assets/svg/projects/one.svg'
-import two from '../assets/svg/projects/two.svg'
-import three from '../assets/svg/projects/three.svg'
-import four from '../assets/svg/projects/four.svg'
-import five from '../assets/svg/projects/five.svg'
-import six from '../assets/svg/projects/six.svg'
-import seven from '../assets/svg/projects/seven.svg'
-import eight from '../assets/svg/projects/eight.svg'
-
 import Calculator from '../assets/Images/Calculator.png'
 import WeatherForecast from '../assets/Images/WeatherForecast.png'
 import Visitly from '../assets/Images/Visitly.png'
